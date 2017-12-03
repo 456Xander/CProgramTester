5f4b6614fac0a11b3c5a3a0becb197da44d8305d
ChrissiF <chrissi1997@gmx.net>
2017-12-03 13:46:33 +0100

    Some Changes

--- src/main/java/at/crimsonbit/cprogramtester/programs/Program.java
+++ src/main/java/at/crimsonbit/cprogramtester/programs/Program.java
@@ -3,6 +3,7 @@ package at.crimsonbit.cprogramtester.programs;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class Program {
 	
@@ -11,13 +12,25 @@ private File programFolder;
 private String name;
 private String projectFolder;
 
-}
+
 
 public String programName(String name) {
 	BufferedWriter bw;
 	File f = new File(projectFolder+"programName.c");
 	
-	FileWriter fw = new FileWriter(f);
+	FileWriter fw = null;
+	try {
+		fw = new FileWriter(f);
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
 	bw = new BufferedWriter(fw);
-	bw.write(name);
+	try {
+		bw.write(name);
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	
+	return name;
+}
 }
